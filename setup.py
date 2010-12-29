92d3ccef17969133282a9a1e1fcd8a72357fed06
Anand Patil <anand.prabhakar.patil@gmail.com>
2010-12-29 06:44:18 -0800

    ...

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ config = Configuration('mbgw',parent_package=None,top_path=None)
 
 config.packages = ["mbgw"]
 
+
 if __name__ == '__main__':
     from numpy.distutils.core import setup
     setup(**(config.todict()))
